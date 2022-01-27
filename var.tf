4075051a455129f7f97b2b6dbf81d8b0e4c675de
Kari <Jagadeesh.Kari@bshg.com>
2022-01-27 12:49:58 +0530

    updating vpc and var files

---
+++ var.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type = string
+  default = "ap-southeast-1"
+}
+
+variable "access_key" {
+  type = string
+  default = "AKIA24JKMMN3W6NLYPVD"
+}
+
+variable "secret_key" {
+  type = string
+  default = "xUfUEQ5j2WZDY39cAAUF2iQNgh1MTfs1KaYdMZOp"
+}
